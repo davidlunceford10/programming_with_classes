c841b4317eaea54a284dd24fa840bbf24f75a9ad
David Lunceford <davidlunceford10@yahoo.com>
2024-06-12 17:24:10 -0600

    Grade Program C#

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userGradePercentage = Console.ReadLine();
+        float userPercentage = float.Parse(userGradePercentage);
+        string letter;
+
+        if (userPercentage >= 90)
+        {
+            letter = "A"; 
+            Console.WriteLine($"{letter}");
+        }
+
+        else if (userPercentage >= 80 && userPercentage < 90)
+        {
+            letter = "B"; 
+            Console.WriteLine($"{letter}");
+        }
+
+        else if (userPercentage >= 70 && userPercentage < 80)
+        {
+            letter = "C"; 
+            Console.WriteLine($"{letter}");
+        }
+
+        else if (userPercentage >= 60 && userPercentage < 70)
+        {
+            letter = "D"; 
+            Console.WriteLine($"{letter}");
+        }
+
+        else if (userPercentage < 60)
+        {
+            letter = "F"; 
+            Console.WriteLine($"{letter}");
+        }
+
+        else
+        {
+            Console.WriteLine("That is invalid input.");
+        }
+
+        if (userPercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass, best of luck on your second goaround!");
+        }
+
     }
 }
