95f298aee5dac6e0666f0e3744c8dd7480e66ff2
David Lunceford <davidlunceford10@yahoo.com>
2024-06-13 16:00:31 -0600

    number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 100);
+        
+        Console.Write("What is your guess? ");
+        string numberGuess = Console.ReadLine();
+        
+        int magicNumberGuessInt = int.Parse(numberGuess);
+
+        if (magicNumber > magicNumberGuessInt)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < magicNumberGuessInt)
+            {    
+                Console.WriteLine("Lower");
+            }   
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        do
+        {   
+            Console.Write("What is your guess? ");
+            numberGuess = Console.ReadLine();
+            magicNumberGuessInt = int.Parse(numberGuess);
+
+            if (magicNumber > magicNumberGuessInt)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < magicNumberGuessInt)
+            {    
+                Console.WriteLine("Lower");
+            }   
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }while (magicNumberGuessInt != magicNumber);
+        
     }
 }
