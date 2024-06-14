2373d1a6e8f89905b9718809b8f9eaf01ab2da71
David Lunceford <davidlunceford10@yahoo.com>
2024-06-14 17:52:31 -0600

    Prep 3 and 4 code completion

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        int userNumber = -1;
+        Console.Write("Enter a number, type 0 to quit: ");
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number: ");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {max}");
     }
 }
