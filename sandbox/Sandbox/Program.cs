c4ec80099e78518d54752861f8fadda12f62fa79
David Lunceford <davidlunceford10@yahoo.com>
2024-07-08 14:57:13 -0600

    Polymorphism Activity

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,14 +1,32 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program 
 {
     static void Main(string[] args)
     {
-        Person person1 = new Person("Bob", "Billy", 24, 210);
-        Console.WriteLine(person1.PersonInformation());
-        Fireman fireman = new Fireman("EMT and Parametic", "Billiam", "Martin", 24, 210);
-        Console.WriteLine(fireman.FiremanInformation());
-    
+        Employee employee = new Employee("Bob", 40, 23.77);
+
+        // Console.WriteLine($"{employee.GetName()} is due: ${employee.GetPay()}");
+
+        ConstructionWorker constructionWorker = new ConstructionWorker("Betty", 40, 35.8);
+        // Console.WriteLine($"{constructionWorker.GetName()} is due: ${constructionWorker.GetPay()}");
+
+        Doctor doctor = new Doctor("Belinda", 240000.99);
+        // Console.WriteLine($"{doctor.GetName()} is due: ${doctor.GetPay()}");
+
+        List<Employee> employees = new List<Employee>();
+        employees.Add(employee);
+        employees.Add(constructionWorker);
+        employees.Add(doctor);
+
+        foreach (Employee e in employees)
+        {
+            Console.WriteLine($"{e.GetName()} is due: {e.GetPay()}");
+
+        }
+
+    //polymorphism activity
     }
 }
 
